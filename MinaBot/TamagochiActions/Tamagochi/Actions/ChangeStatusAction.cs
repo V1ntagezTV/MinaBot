9098c8364bca2be2f8b297fb64cc27c15526e39f
V1ntagezTV <insur.kunafin@yandex.ru>
2020-11-06 02:50:08 +0500

    Add new actions: ChangeStatusAction.cs TakeOffItemAction.cs

---
+++ MinaBot/TamagochiActions/Tamagochi/Actions/ChangeStatusAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using MinaBot.Models;
+
+namespace MinaBot.BotTamagochi.MVC.Tamagochi.Actions
+{
+    public class ChangeStatusAction : APetActionCommand
+    {
+        public ChangeStatusAction(TamagochiModel pet, CommandModel command)
+            : base(pet, command, true) { }
+
+        public override string[] Options => new[] { "status", "s" };
+        public override MessageResult Invoke()
+        {
+            var newStatus = string.Join(" ", Command.GetArgs);
+            if (newStatus.Length >= 20)
+            {
+                return new MessageResult.ErrorView("Max status length equals 20.");
+            }
+            Pet.CurrentStatus = newStatus;
+            return new MessageResult.BooleanView(true);
+        }
+    }
+}
---
+++ MinaBot/TamagochiActions/Tamagochi/Actions/TakeOffItemAction.cs
@@ -0,0 +1,28 @@
+﻿using MinaBot.BotTamagochi.ItemsPack;
+using MinaBot.Models;
+
+namespace MinaBot.BotTamagochi.MVC.Tamagochi.Actions
+{
+    public class TakeOffItemAction : APetActionCommand
+    {
+        public TakeOffItemAction(TamagochiModel pet, CommandModel command)
+            : base(pet, command, true) { }
+        
+        public override string[] Options => new[] { "takeoff", "to" };
+        public override MessageResult Invoke()
+        {
+            var clothesType = Command.GetArgs[0];
+            switch (clothesType)
+            {
+                case "h":
+                    Pet.Backpack.Add(Pet.HatID.ToString());
+                    Pet.HatID = ItemMocks.defaultCleanItem.ID;
+                    break;
+                default:
+                    break;
+            }
+
+            return new MessageResult.BooleanView(true);
+        }
+    }
+}
