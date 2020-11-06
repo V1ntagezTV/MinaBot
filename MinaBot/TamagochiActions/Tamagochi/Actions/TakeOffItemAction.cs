2ea68142aec9e37022ae6bbad09f2dadd9b066da
V1ntagezTV <insur.kunafin@yandex.ru>
2020-11-06 05:25:11 +0500

    Take off all itemTypes added.

--- MinaBot/TamagochiActions/Tamagochi/Actions/TakeOffItemAction.cs
+++ MinaBot/TamagochiActions/Tamagochi/Actions/TakeOffItemAction.cs
@@ -18,10 +18,23 @@ namespace MinaBot.BotTamagochi.MVC.Tamagochi.Actions
                     Pet.Backpack.Add(Pet.HatID.ToString());
                     Pet.HatID = ItemMocks.defaultCleanItem.ID;
                     break;
+                case "j":
+                    Pet.Backpack.Add(Pet.JacketID.ToString());
+                    Pet.JacketID = ItemMocks.defaultCleanItem.ID;
+                    break;
+                case "p":
+                    Pet.Backpack.Add(Pet.PantsID.ToString());
+                    Pet.PantsID = ItemMocks.defaultCleanItem.ID;
+                    break;
+                case "b":
+                    Pet.Backpack.Add(Pet.BootsID.ToString());
+                    Pet.BootsID = ItemMocks.defaultCleanItem.ID;
+                    break;
+                
                 default:
+                    return new MessageResult.BooleanView(false);
                     break;
             }
-
             return new MessageResult.BooleanView(true);
         }
     }
