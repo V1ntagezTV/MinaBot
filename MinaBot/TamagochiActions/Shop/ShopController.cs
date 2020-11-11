067855ad796b1f7a4e913aa236a5b428a9ece7f2
V1ntagezTV <insur.kunafin@yandex.ru>
2020-11-11 20:43:54 +0500

    create shop directory and functional

---
+++ MinaBot/TamagochiActions/Shop/ShopController.cs
@@ -0,0 +1,16 @@
+﻿using MinaBot.BotTamagochi.MVC.Tamagochi.Actions;
+using MinaBot.Main;
+using MinaBot.Models;
+
+namespace MinaBot.TamagochiActions.Shop
+{
+    public class ShopController : IController
+    {
+        public CommandModel Command { get; }
+        public AActionCommand[] Actions { get; }
+        public MessageResult GetResult()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ MinaBot/TamagochiActions/Shop/ShopModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MinaBot.TamagochiActions.Shop
+{
+    public class ShopModel
+    {
+        public DateTime UpdateDate;
+        public int[] GeneratedItems;
+        public char Separator = ' ';
+    }
+}
