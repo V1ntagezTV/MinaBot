92b4be0a5bf6e5df34c7569ef4f1ca0724ca752f
V1ntagezTV <insur.kunafin@yandex.ru>
2020-11-29 20:37:33 +0500

    qoutes work with new database

--- MinaBot/Entity/Models/QuoteModel.cs
+++ MinaBot/Entity/Models/QuoteModel.cs
@@ -1,9 +1,12 @@
-﻿namespace MinaBot.DefaultActions.Models
+﻿using Discord;
+using System;
+
+namespace MinaBot.DefaultActions.Models
 {
     public class QuoteModel
     {
         public int Id { get; set; }
-        public UserModel Author { get; set; }
+        public User Author { get; set; }
         public string Prefix { get; set; }
         public string Text { get; set; }
         public string Desc { get; set; }
