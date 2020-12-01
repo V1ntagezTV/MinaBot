3b744cd7f202bb9e4433ac5a9c5b9fc3214ef5dc
V1ntagezTV <insur.kunafin@yandex.ru>
2020-12-01 14:17:24 +0500

    make database single finish commit

--- MinaBot/TamagochiActions/Tamagochi/Models/Characteristics/Happiness.cs
+++ MinaBot/TamagochiActions/Tamagochi/Models/Characteristics/Happiness.cs
@@ -31,7 +31,7 @@ namespace MinaBot.BotTamagochi.MVC.Tamagochi.Characteristics
                 }
             }
         }
-        public override double MinusEveryMinute => 0.1;
+        public override double MinusEveryMinute => 0.3;
         public override DateTime LastConsume { get; set; }
     }
 }
