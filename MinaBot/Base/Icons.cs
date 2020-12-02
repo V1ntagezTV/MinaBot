962064ec54907bb83b77b03b06422453649fb290
V1ntagezTV <insur.kunafin@yandex.ru>
2020-12-02 05:42:32 +0500

    add icons and in help view

--- MinaBot/Base/Icons.cs
+++ MinaBot/Base/Icons.cs
@@ -4,5 +4,7 @@
     {
         public const string Coins = "<a:coins:781827813715869696>";
         public const string Level = "<:happines:781836925492264960>";
+        public const string Star = "<:level:781836925492264960>";
+        public const string Link = "<:link:783481173434695680>";
     }
 }
